e5d3c443bdd2789962d2b901261352e35e405774
Tom Dörr <tomdoerr96@gmail.com>
2025-05-03 09:00:39 +0200

    Add .windsurfrules

--- agent_components/__init__.py
+++
@@ -1,8 +0,0 @@
-# agent_components/__init__.py
-
-# Make the core components easily importable from the package
-from .io_handler import AgentIO, ConsoleIO
-from .action_executor import ActionExecutor
-
-# Optional: Define what gets imported with 'from agent_components import *'
-__all__ = ["AgentIO", "ConsoleIO", "ActionExecutor"]
